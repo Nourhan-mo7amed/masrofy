144ddcec5b44f3ee365ecb9f894dd42fa079d681
SalmaYounissFCI <salmayounis680@gmail.com>
2025-08-31 02:05:48 +0300

    "Add user_model"

---
+++ lib/models/user_model.dart
@@ -0,0 +1,13 @@
+class UserModel {
+  String id;
+  String name;
+  String email;
+  String password;
+
+  UserModel({
+    required this.id,
+    required this.name,
+    required this.email,
+    required this.password,
+  });
+}
