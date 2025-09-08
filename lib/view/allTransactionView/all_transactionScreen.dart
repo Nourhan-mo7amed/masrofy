792c2ddf72d1624dc9bb228decb3a870b298aae0
SalmaYounissFCI <salmayounis680@gmail.com>
2025-09-09 02:32:16 +0300

    connect AllTransactionScreen with TransactionViewmodel and improve UI
    - Integrated TransactionViewmodel with Provider
    - Added tab-based filtering for expenses and income
    - Implemented ListView.builder to display transactions
    - Improved transaction card design (Card + ListTile with icons)
    - Used intl package for formatted date display
    - Added empty state when no transactions are available

--- lib/view/allTransactionView/all_transactionScreen.dart
+++ lib/view/allTransactionView/all_transactionScreen.dart
@@ -1,7 +1,14 @@
+import 'dart:math';
+import 'package:intl/intl.dart';
+
 import 'package:flutter/material.dart';
+import 'package:masrofy/core/constants/colors_app.dart';
 import 'package:masrofy/l10n/app_localizations.dart';
+import 'package:masrofy/models/transaction_model.dart';
+import 'package:provider/provider.dart';
 // import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import '../../widgets/tapButtom.dart';
+import '../../viewmodels/transaction_viewModel.dart';
 
 class AlltransactionScreen extends StatefulWidget {
   const AlltransactionScreen({super.key});
@@ -15,6 +22,14 @@ class _AlltransactionScreen extends State<AlltransactionScreen> {
   @override
   Widget build(BuildContext context) {
     final loc = AppLocalizations.of(context)!;
+
+    final txViewModel = Provider.of<TransactionViewmodel>(context);
+    final transactions = txViewModel.transactions;
+
+    // فلترة حسب التاب
+    final filteredTransactions = selectedTab == 0
+        ? txViewModel.expenses
+        : txViewModel.incomes;
 
     return Scaffold(
       appBar: PreferredSize(
@@ -70,6 +85,70 @@ class _AlltransactionScreen extends State<AlltransactionScreen> {
               ),
             ),
             const SizedBox(height: 20),
+
+            Expanded(
+              child: filteredTransactions.isEmpty
+                  ? Center(child: Text("No transactions found"))
+                  : ListView.builder(
+                      padding: const EdgeInsets.all(8),
+                      itemCount: filteredTransactions.length,
+                      itemBuilder: (context, index) {
+                        final tx = filteredTransactions[index];
+                        final isExpense = tx.type == "expense";
+                        final formattedDate = DateFormat(
+                          'dd/MM/yyyy',
+                        ).format(tx.date);
+                        return Card(
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(12),
+                          ),
+                          margin: const EdgeInsets.symmetric(vertical: 8),
+                          elevation: 3,
+                          child: ListTile(
+                            contentPadding: const EdgeInsets.symmetric(
+                              horizontal: 16,
+                              vertical: 8,
+                            ),
+                            leading: CircleAvatar(
+                              radius: 25,
+                              backgroundColor: isExpense
+                                  ? AppColors.expense.withOpacity(0.1)
+                                  : AppColors.income.withOpacity(0.1),
+                              child: Icon(
+                                isExpense
+                                    ? Icons.arrow_upward
+                                    : Icons.arrow_downward,
+                                color: isExpense
+                                    ? AppColors.expense
+                                    : AppColors.income,
+                              ),
+                            ),
+                            title: Text(
+                              tx.title,
+                              style: const TextStyle(
+                                fontWeight: FontWeight.bold,
+                                fontSize: 16,
+                              ),
+                            ),
+                            subtitle: Text(
+                              formattedDate,
+                              style: const TextStyle(color: Colors.grey),
+                            ),
+                            trailing: Text(
+                              "${tx.amount.toStringAsFixed(2)}",
+                              style: TextStyle(
+                                color: isExpense
+                                    ? AppColors.expense
+                                    : AppColors.income,
+                                fontWeight: FontWeight.bold,
+                                fontSize: 16,
+                              ),
+                            ),
+                          ),
+                        );
+                      },
+                    ),
+            ),
           ],
         ),
       ),
