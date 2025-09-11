1283140c71b98136fbb9b741507cb293738ea233
SalmaYounissFCI <salmayounis680@gmail.com>
2025-09-12 01:54:54 +0300

    Refactor TransactionModel: unify income and expense into TransactionModel - optional fields: categoryId (for expense), source (for income) - added toJson/fromJson and Firestore factory

--- lib/models/transaction_model.dart
+++ lib/models/transaction_model.dart
@@ -1,17 +1,52 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+
 class TransactionModel {
+  final String id;
   final String title;
   final double amount;
   final DateTime date;
   final String? notes;
   final String type;
-  final int? categoryIndex;
+  final String? categoryId;
+  final String? source;
 
   TransactionModel({
+    required this.id,
+    this.categoryId,
+    this.source,
     required this.title,
     required this.amount,
     required this.date,
     this.notes,
     required this.type,
-    required this.categoryIndex,
   });
+  Map<String, dynamic> toJson() => {
+    "id": id,
+    "title": title,
+    "amount": amount,
+    "categoryId": categoryId,
+    "date": date.toIso8601String(),
+    "notes": notes,
+    "type": type,
+    "source": source,
+  };
+  factory TransactionModel.fromJson(Map<String, dynamic> json, String id) =>
+      TransactionModel(
+        id: id,
+        title: json["title"] ?? "",
+        amount: (json["amount"] as num).toDouble(),
+        date: DateTime.parse(json["date"]),
+        notes: json["notes"],
+        type: json["type"],
+        categoryId: json["categoryId"],
+        source: json["source"],
+      );
+
+  /// 🟢 طريقة مباشرة للـ Firestore DocumentSnapshot
+  factory TransactionModel.fromFirestore(
+    DocumentSnapshot<Map<String, dynamic>> doc,
+  ) {
+    final data = doc.data()!;
+    return TransactionModel.fromJson(data, doc.id);
+  }
 }
