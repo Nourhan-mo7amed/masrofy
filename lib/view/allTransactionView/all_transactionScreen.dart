8a9a44df41ac2a9100fc396ec168d6d850d89ea9
SalmaYounissFCI <salmayounis680@gmail.com>
2025-09-12 21:59:58 +0300

    feat: implement transaction filtering using streams in AlltransactionScreen

--- lib/view/allTransactionView/all_transactionScreen.dart
+++ lib/view/allTransactionView/all_transactionScreen.dart
@@ -2,6 +2,7 @@ import 'package:intl/intl.dart';
 import 'package:flutter/material.dart';
 import 'package:masrofy/core/constants/colors_app.dart';
 import 'package:masrofy/l10n/app_localizations.dart';
+import 'package:masrofy/models/transaction_model.dart';
 import 'package:provider/provider.dart';
 // import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import '../../widgets/tapButtom.dart';
@@ -21,11 +22,6 @@ class _AlltransactionScreen extends State<AlltransactionScreen> {
     final loc = AppLocalizations.of(context)!;
 
     final txViewModel = Provider.of<TransactionViewmodel>(context);
-
-    // فلترة حسب التاب
-    final filteredTransactions = selectedTab == 0
-        ? txViewModel.expenses
-        : txViewModel.incomes;
 
     // 🟢 Mapping categories & sources
     final categoryIcons = {
@@ -97,69 +93,82 @@ class _AlltransactionScreen extends State<AlltransactionScreen> {
             const SizedBox(height: 20),
 
             Expanded(
-              child: filteredTransactions.isEmpty
-                  ? Center(child: Text("No transactions found"))
-                  : ListView.builder(
-                      padding: const EdgeInsets.all(8),
-                      itemCount: filteredTransactions.length,
-                      itemBuilder: (context, index) {
-                        final tx = filteredTransactions[index];
-                        final isExpense = tx.type == "expense";
-                        final formattedDate = DateFormat(
-                          'dd/MM/yyyy',
-                        ).format(tx.date);
-                        return Card(
-                          shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(12),
+              child: StreamBuilder<List<TransactionModel>>(
+                stream: selectedTab
+                    ? txViewModel.incomesStream
+                    : txViewModel.expensesStream,
+                builder: (context, snapshot) {
+                  if (snapshot.connectionState == ConnectionState.waiting) {
+                    return const Center(child: CircularProgressIndicator());
+                  }
+                  if (!snapshot.hasData || snapshot.data!.isEmpty) {
+                    return const Center(child: Text("No transactions found"));
+                  }
+                  final filteredTransactions = snapshot.data!;
+
+                  return ListView.builder(
+                    padding: const EdgeInsets.all(8),
+                    itemCount: filteredTransactions.length,
+                    itemBuilder: (context, index) {
+                      final tx = filteredTransactions[index];
+                      final isExpense = tx.type == "expense";
+                      final formattedDate = DateFormat(
+                        'dd/MM/yyyy',
+                      ).format(tx.date);
+                      return Card(
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(12),
+                        ),
+                        margin: const EdgeInsets.symmetric(vertical: 8),
+                        elevation: 3,
+                        child: ListTile(
+                          contentPadding: const EdgeInsets.symmetric(
+                            horizontal: 16,
+                            vertical: 8,
                           ),
-                          margin: const EdgeInsets.symmetric(vertical: 8),
-                          elevation: 3,
-                          child: ListTile(
-                            contentPadding: const EdgeInsets.symmetric(
-                              horizontal: 16,
-                              vertical: 8,
-                            ),
-                            leading: CircleAvatar(
-                              radius: 25,
-                              backgroundColor: isExpense
-                                  ? AppColors.expense.withOpacity(0.1)
-                                  : AppColors.income.withOpacity(0.1),
-                              child: Icon(
-                                isExpense
-                                    ? categoryIcons[tx.categoryId] ??
-                                          Icons.category
-                                    : sourceIcons[tx.source] ??
-                                          Icons.attach_money,
-                                color: isExpense
-                                    ? AppColors.expense
-                                    : AppColors.income,
-                              ),
-                            ),
-                            title: Text(
-                              tx.title,
-                              style: const TextStyle(
-                                fontWeight: FontWeight.bold,
-                                fontSize: 16,
-                              ),
-                            ),
-                            subtitle: Text(
-                              formattedDate,
-                              style: const TextStyle(color: Colors.grey),
-                            ),
-                            trailing: Text(
-                              "${isExpense ? '-' : '+'}${tx.amount.toStringAsFixed(2)} EGP",
-                              style: TextStyle(
-                                color: isExpense
-                                    ? AppColors.expense
-                                    : AppColors.income,
-                                fontWeight: FontWeight.bold,
-                                fontSize: 16,
-                              ),
+                          leading: CircleAvatar(
+                            radius: 25,
+                            backgroundColor: isExpense
+                                ? AppColors.expense.withOpacity(0.1)
+                                : AppColors.income.withOpacity(0.1),
+                            child: Icon(
+                              isExpense
+                                  ? categoryIcons[tx.categoryId] ??
+                                        Icons.category
+                                  : sourceIcons[tx.source] ??
+                                        Icons.attach_money,
+                              color: isExpense
+                                  ? AppColors.expense
+                                  : AppColors.income,
                             ),
                           ),
-                        );
-                      },
-                    ),
+                          title: Text(
+                            tx.title,
+                            style: const TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontSize: 16,
+                            ),
+                          ),
+                          subtitle: Text(
+                            formattedDate,
+                            style: const TextStyle(color: Colors.grey),
+                          ),
+                          trailing: Text(
+                            "${isExpense ? '-' : '+'}${tx.amount.toStringAsFixed(2)} EGP",
+                            style: TextStyle(
+                              color: isExpense
+                                  ? AppColors.expense
+                                  : AppColors.income,
+                              fontWeight: FontWeight.bold,
+                              fontSize: 16,
+                            ),
+                          ),
+                        ),
+                      );
+                    },
+                  );
+                },
+              ),
             ),
           ],
         ),
